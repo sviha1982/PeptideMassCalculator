68c64e23af42bfedcf49e95f680a4b7b82d7adb2
sviha1982 <sviha@gmx.de>
2021-03-14 18:48:33 -0700

    fixed calculation issues

--- src/calculations.py
+++ src/calculations.py
@@ -1,14 +1,36 @@
 import pandas as pd
 import streamlit as st
+import re
 
-def calculate_ms(ms, sequence, ms_type: str) -> float:
-    sum_ms: float = 0
+def calculate_ms(ms, sequence) -> (float, float):
+    sum_mono: float = 0
+    sum_avg: float = 0
+
+    delimiter_list = [s.start() for s in re.finditer('$', sequence)]
+    for delimiter in range(0,len(delimiter_list),2):
+        start = delimiter
+        end = delimiter +1
+        sub_sequence = sequence[start:end]
+        unusual_ms = ms[ms["character"] == sub_sequence].reset_index()
+
+        if unusual_ms.empty:
+            continue
+
+        sum_mono += unusual_ms["mono"][0]
+        sum_avg += unusual_ms["avg"][0]
+
+    for delimiter in range(0,len(delimiter_list),2):
+        start = delimiter
+        end = delimiter +1
+        if len(sequence) >= end:
+            sequence = sequence[0: start:] + sequence[end + 1::]
 
     for char in sequence.upper():
         temp_ms_df = ms[ms["character"] == char].reset_index()
-        sum_ms = sum_ms + temp_ms_df[ms_type][0]
+        sum_mono = sum_mono + temp_ms_df["mono"][0]
+        sum_avg = sum_avg + temp_ms_df["avg"][0]
 
-    return sum_ms
+    return sum_mono, sum_avg
 
-def calculate_total_ms(sum_ms: float, c_ms: float, n_ms: float, ms_type) -> float:
-    return sum_ms + c_ms + n_ms
+def calculate_total_ms(sum_ms: float, c_ms: float, n_ms: float) -> float:
+        return sum_ms + c_ms + n_ms
--- src/main.py
+++ src/main.py
@@ -4,12 +4,27 @@ import numpy as np
 from file_handler import load_df, create_data, validate_input, load_cache_df, get_ms
 from calculations import calculate_ms, calculate_total_ms
 import os
+import math
 
-aa_mass_df = load_cache_df("aa_residues.csv")
+aa_mass_df = load_cache_df("character.csv")
 n_option_df = load_df("n-option.csv")
 c_option_df = load_df("c-option.csv")
+
 sequence = st.text_input("One-character symbols are used for natural amino acids and numbers for other residues",
                          "")
+if sequence == "":
+    st.write("insert sequence")
+
+u_mono = None
+U_avg = None
+if st.checkbox("show unnatural amino acids"):
+    st.write(aa_mass_df[19:-1])
+    unnatural = st.text_input("Add unnatural amino acid", "")
+    if unnatural is not None:
+        u_mono, u_avg, aa_mass_df = validate_input(aa_mass_df, unnatural, "character")
+    else:
+        st.write("Type name, monoisotopic and average mass (e.g. Any, 101.0603, 101.1250")
+
 col_n, col_c = st.beta_columns(2)
 n_mono = None
 n_avg = None
@@ -36,15 +51,14 @@ else:
     c_mono, c_avg = get_ms(c_option_df, c_option, "c-option")
 
 
-#TODO: figure out how to get selectbox in 2 columns
-
-if n_mono is None or c_mono is None:
+if math.isnan(n_mono) or math.isnan(c_mono) or sequence == "":
    st.write("Insert values")
 
 else:
     mono_ms_df, avg_ms_df = create_data(aa_mass_df)
-    sum_mono = calculate_ms(mono_ms_df, sequence, "mono")
-    sum_avg = calculate_ms(avg_ms_df, sequence, "avg")
+    sum_mono, sum_avg = calculate_ms(aa_mass_df, sequence)
 
-    st.write("monoisotopic mass:", "{:12.4f}".format(calculate_total_ms(sum_mono, n_mono, c_mono, "mono")))
-    st.write("average mass:", "{:12.4f}".format(calculate_total_ms(sum_avg, n_avg, c_avg, "avg")))
+
+
+    st.write("monoisotopic mass:", "{:12.4f}".format(calculate_total_ms(sum_mono, n_mono, c_mono)))
+    st.write("average mass:", "{:12.4f}".format(calculate_total_ms(sum_avg, n_avg, c_avg)))
